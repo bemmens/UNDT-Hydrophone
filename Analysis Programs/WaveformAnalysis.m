1ff30143a59ee2570b94149bc5605bbe59306cef
bemmens <bjfemmens@gmail.com>
2024-05-23 12:12:24 +0100

    added checklist, rast and scp save

--- Analysis Programs/WaveformAnalysis.m
+++ Analysis Programs/WaveformAnalysis.m
@@ -1,9 +1,11 @@
 %% Plot waveforms
 
+%% Load Data
 % Consider clearing workspace to relieve RAM
 folder_path = 'C:\Users\gv19838\OneDrive - University of Bristol\PhD\Hydrophone\UNDT-Hydrophone\DataOut\';
 file_name = 'test';
 path = strcat(folder_path,file_name,'.mat');
-%load(path)
+load(path)
 
+%% Plot
 plot(squeeze(scanData(1,1,1:1e6,:)))
