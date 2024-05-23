06743c7193672abaa96bf6eca63e0fedbdeb7c8c
bemmens <bjfemmens@gmail.com>
2024-05-23 10:00:43 +0100

    Fixed pixel locations relative to ticks

--- Analysis Programs/PeakVoltageAnalysis.m
+++ Analysis Programs/PeakVoltageAnalysis.m
@@ -1,12 +1,17 @@
 %% Peak Voltage Analysis
 % For reference: scanData = [x,y,samples,chanel];
 
+%% Load Data
 folder_path = 'C:\Users\gv19838\OneDrive - University of Bristol\PhD\Hydrophone\UNDT-Hydrophone\DataOut\';
 file_name = 'test';
 path = strcat(folder_path,file_name,'.mat');
 load(path)
 
+%% Extract Data
 Vpk = squeeze(max(scanData(:,:,:,1),[], 3));
-pcolor(xs,ys,Vpk)
+
+%% Plot
+imagesc(xs,ys,Vpk)
+colorbar
 xlabel('x (mm)')
 ylabel('y (mm)')
